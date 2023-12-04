94f5664929cb404a949187b0c1823bec1ab2540a
shubh patni <shubhpatni2002@gmail.com>
2023-12-05 01:23:59 +0530

    removed edge stuff

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -2,7 +2,7 @@ import { authOptions } from "@/lib/authOptions";
 import NextAuth from "next-auth";
 export const maxDuration = 30;
 
-export const runtime = "edge";
+//export const runtime = "edge";
 
 const handler = NextAuth(authOptions);
 export { handler as GET, handler as POST };
