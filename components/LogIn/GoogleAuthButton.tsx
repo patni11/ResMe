7601629d8d62a8589d623afac9901f7b1312a8ab
shubh patni <shubhpatni2002@gmail.com>
2023-12-06 22:21:16 +0530

    to handle auth error added toast

--- components/LogIn/GoogleAuthButton.tsx
+++ components/LogIn/GoogleAuthButton.tsx
@@ -4,16 +4,29 @@ import { signIn } from "next-auth/react";
 import { useSearchParams } from "next/navigation";
 import { Mail } from "lucide-react";
 import { Button } from "@/components/ui/button";
+import { useToast } from "../ui/use-toast";
 const GoogleButton = () => {
   const searchParams = useSearchParams()!;
   let callbackUrl = searchParams.get("callbackUrl")!;
   if (callbackUrl === null) {
     callbackUrl = "/";
   }
+
+  const { toast } = useToast();
+
   return (
     <Button
       variant="outline"
-      onClick={() => signIn("google", { callbackUrl })}
+      onClick={async () => {
+        try {
+          await signIn("google", { callbackUrl, redirect: false });
+        } catch (e) {
+          console.log("There was an error please try again", e);
+          toast({
+            title: "There was some error, Please try again",
+          });
+        }
+      }}
       className="w-full"
       type="button"
     >
